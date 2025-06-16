834f1e7ef4d04733b922f04056acc600831f4140
prajwolK <kprajowl22@tbc.edu.np>
2025-06-16 13:29:16 +0545

    new page file created

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+export const Home = () => {
+  return (
+    <div className="min-h screen bg-background text-foreground overflow-x-hidden">
+      {/* {Theme Toggle} */}
+      {/* {Background Effect} */}
+      {/* {Navbar} */}
+      {/* {Main Content} */}
+      {/* {Footer} */}
+    </div>
+  );
+};
