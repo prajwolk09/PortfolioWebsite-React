b558a36694fed65daf296fd37a07123ea6fb14f9
prajwolK <kprajowl22@tbc.edu.np>
2025-06-17 14:17:44 +0545

    footer section implemented

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { AboutSection } from "../components/AboutSection";
 import { ContactSection } from "../components/ContactSection";
+import { FooterSection } from "../components/FooterSection";
 import { HeroSection } from "../components/HeroSection";
 import { Navbar } from "../components/Navbar";
 import { NoiseBackground } from "../components/NoiseBackground";
@@ -30,6 +31,7 @@ export const Home = () => {
       </main>
 
       {/* Footer */}
+      <FooterSection/>
     </div>
   );
 };
